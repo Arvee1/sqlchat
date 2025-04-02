78ed10a4e88949c25a0600834dfd1b21e926df49
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2025-04-02 20:23:29 +1000

    Update sqlchat.py

--- sqlchat.py
+++ sqlchat.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import sqlite3 as sql
+import pandas as pd
 
 st.write("Hello World")
 
-# connection = sqlite3.connect('Chinook.db')
-# cursor = connection.cursor() #cursor object
-# with open('CEM3_Slice_20180622.sql', 'r') as f: #Not sure if the 'r' is necessary, but recommended.
-#     cursor.executescript(f.read())
+connection = sqlite3.connect('Chinook.db')
+cursor = connection.cursor() #cursor object
+with open('Chinook_Sqlite.sql', 'r') as f: #Not sure if the 'r' is necessary, but recommended.
+     cursor.executescript(f.read())
+
+st.write("after create database")
+
+df = pd.read_sql('SELECT * FROM table LIMIT 10', connection)
