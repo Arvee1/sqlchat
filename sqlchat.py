37bc05f69ab4968bc466579c05d91b7c82de52d7
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2025-04-02 20:21:04 +1000

    Update sqlchat.py

--- sqlchat.py
+++ sqlchat.py
@@ -3,5 +3,7 @@ import sqlite3 as sql
 
 st.write("Hello World")
 
-connection = sqlite3.connect('Chinook.db')
-
+# connection = sqlite3.connect('Chinook.db')
+# cursor = connection.cursor() #cursor object
+# with open('CEM3_Slice_20180622.sql', 'r') as f: #Not sure if the 'r' is necessary, but recommended.
+#     cursor.executescript(f.read())
