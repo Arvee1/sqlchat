128c803f730194de73020c1d3f98bf5e93b8de5a
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2025-04-07 20:47:05 +1000

    Update sqlchatwad.py

--- sqlchatwad.py
+++ sqlchatwad.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st 
 import sqlite3 as sql
 import pandas as pd
 from langchain import hub
@@ -7,6 +7,11 @@ from langchain_community.utilities import SQLDatabase
 from typing_extensions import TypedDict
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from langgraph.prebuilt import create_react_agent
+from typing_extensions import Annotated
+from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+from langgraph.graph import START, StateGraph
+from langgraph.checkpoint.memory import MemorySaver
+from IPython.display import Image, display 
 
 # Set API keys from session state
 openai_api_key = st.secrets["api_key"]
@@ -17,18 +22,18 @@ assert len(prompt_template.messages) == 1
 system_message = prompt_template.format(dialect="SQLite", top_k=5)
 assert len(query_prompt_template.messages) == 1
 
-
-st.write("Create Database")
-
-db = sql.connect('wad2024.db')
+# The UI Part
+st.title("👨‍💻 Wazzup!!!! What do you want to know about about music artists in the Chinook Database?")
+st.write("The Chinook database represents a digital media store, including tables for artists, albums, media tracks, invoices and customers.")
+db = sql.connect('Chinook.db')
 cursor = db.cursor() #cursor object
-with open('wad2024.sql', 'r') as f: #Not sure if the 'r' is necessary, but recommended.
+with open('Chinook_Sqlite.sql', 'r') as f: #Not sure if the 'r' is necessary, but recommended.
      cursor.executescript(f.read())
 
-st.write(cursor.execute("SELECT * FROM General LIMIT 10;"))
+st.write(cursor.execute("SELECT * FROM Artist LIMIT 10;"))
 
-db = SQLDatabase.from_uri("sqlite:///wad2024.db")
-db.run("SELECT * FROM General LIMIT 10;")
+db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+db.run("SELECT * FROM Artist LIMIT 10;")
 
 class State(TypedDict):
   question: str
@@ -36,23 +41,86 @@ class State(TypedDict):
   result: str
   answer: str
 
-toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-tools = toolkit.get_tools()
+llm = init_chat_model("gpt-4o-mini", model_provider="openai", openai_api_key=openai_api_key)
 
-agent_executor = create_react_agent(llm, tools, prompt=system_message)
+query_prompt_template = hub.pull("langchain-ai/sql-query-system-prompt")
 
-st.write("after create database")
+assert len(query_prompt_template.messages) == 1
+query_prompt_template.messages[0].pretty_print()
 
-user_input = input("Get me the list of all the artists.")
-for step in agent_executor.stream(
-     {"messages": [{"role": "user", "content": user_input}]},
-     stream_mode="values",
-):
-     step["messages"][-1].pretty_print()
-     #st.write(step["messages"][-1].pretty_print())
+class QueryOutput(TypedDict):
+    """Generated SQL query."""
 
-st.write("After Stream")
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
 
-# df = pd.read_sql('SELECT * FROM Artist LIMIT 10', db)
-# st.write(df.to_string())
 
+def write_query(state: State):
+    """Generate SQL query to fetch information."""
+    prompt = query_prompt_template.invoke(
+        {
+            "dialect": db.dialect,
+            "top_k": 10,
+            "table_info": db.get_table_info(),
+            "input": state["question"],
+        }
+    )
+    structured_llm = llm.with_structured_output(QueryOutput)
+    result = structured_llm.invoke(prompt)
+    return {"query": result["query"]}
+
+def execute_query(state: State):
+    """Execute SQL query."""
+    execute_query_tool = QuerySQLDatabaseTool(db=db)
+    return {"result": execute_query_tool.invoke(state["query"])}
+
+def generate_answer(state: State):
+    """Answer question using retrieved information as context."""
+    prompt = (
+        "Given the following user question, corresponding SQL query, "
+        "and SQL result, answer the user question.\n\n"
+        f'Question: {state["question"]}\n'
+        f'SQL Query: {state["query"]}\n'
+        f'SQL Result: {state["result"]}'
+    )
+    response = llm.invoke(prompt)
+    return {"answer": response.content}
+
+def generate_answer(state: State):
+    """Answer question using retrieved information as context."""
+    prompt = (
+        "Given the following user question, corresponding SQL query, "
+        "and SQL result, answer the user question.\n\n"
+        f'Question: {state["question"]}\n'
+        f'SQL Query: {state["query"]}\n'
+        f'SQL Result: {state["result"]}'
+    )
+    response = llm.invoke(prompt)
+    return {"answer": response.content}
+
+graph_builder = StateGraph(State).add_sequence(
+    [write_query, execute_query, generate_answer]
+)
+graph_builder.add_edge(START, "write_query")
+# graph = graph_builder.compile()
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory, interrupt_before=["execute_query"])
+
+# Now that we're using persistence, we need to specify a thread ID
+# so that we can continue the run after review.
+config = {"configurable": {"thread_id": "1"}}
+# display(Image(graph.get_graph().draw_mermaid_png()))
+
+prompt = st.text_area("Please enter what you want to know about music artists.")
+
+if st.button("Submit to AI", type="primary"):
+     for step in graph.stream(
+         {"question": prompt},
+         config,
+         stream_mode="updates",
+     ):
+         st.write(step)
+     
+     # If approved, continue the graph execution
+     for step in graph.stream(None, config, stream_mode="updates"):
+          st.write(step)
