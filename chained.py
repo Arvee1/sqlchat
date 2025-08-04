78bd4673ae2afb502061eb9e064310439907bbbf
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2025-08-04 11:11:26 +1000

    Update chained.py

--- chained.py
+++ chained.py
@@ -15,8 +15,8 @@ import streamlit as st
 import sys
 
 # sqlite3 workaround
-# __import__('pysqlite3')
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import requests
 import openai
